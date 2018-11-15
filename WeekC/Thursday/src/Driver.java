544526160495ccd036b81d5126ca4094ccb3238e
OdinThunder <naka506@yahoo.com>
2018-11-15 16:51:28 -0500

    calling noargs constructor from Driver

--- WeekC/Thursday/src/Driver.java
+++ WeekC/Thursday/src/Driver.java
@@ -2,5 +2,10 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the point of entry into my  program");
+		//make a car object with no set fields
+		//declare the variable carOne and assign a new Car to it
+		Car carOne = new Car();
+		//toString is a method inherited from Object
+		System.out.println("carOne: "  +carOne.toString());
 	}
 }
